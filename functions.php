ba5629dbe049b914a08028ccbe917fe8b96b3a95
VK <belsynth@gmail.com>
2019-08-14 21:02:03 +0300

    Создал файл functions.php и перенес в него cut_text() из index.php

---
+++ functions.php
@@ -0,0 +1,18 @@
+<?php
+function cut_text($text, $length = 300)
+{
+    if (mb_strlen($text) > $length) {
+        $text_array = explode(' ', $text);
+        $length_sum = 0;
+        $result_array = [];
+        foreach ($text_array as $word) {
+            $length_sum += mb_strlen($word) + 1;
+            if ($length_sum < $length) {
+                $result_array[] = $word;
+            }
+        }
+        return '<p>' . implode(' ', $result_array) . '...' .
+            '</p><a class="post-text__more-link" href="#">Читать далее</a>';
+    }
+    return '<p>' . $text . '</p>';
+}
