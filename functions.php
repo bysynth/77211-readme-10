bbb1b4a8b0df0fabe833b9aef720c593c6ba8e4c
VK <belsynth@gmail.com>
2019-08-17 14:31:44 +0300

    Изменил функцию get_relative_time_format, убрал лишнюю вложеность условий

--- functions.php
+++ functions.php
@@ -33,32 +33,28 @@ function get_relative_time_format($time_data)
     $diff_days_count = (int)date_interval_format($dt_diff, '%a');
     $diff_month_count = (int)date_interval_format($dt_diff, '%m');
 
-    if ($diff_days_count === 0) {
-        if ($diff_minutes_count < 60 && $diff_hours_count === 0) {
-            return $diff_minutes_count . ' ' . get_noun_plural_form($diff_minutes_count, 'минута', 'минуты',
-                    'минут') . ' назад';
-        }
-
-        if ($diff_hours_count < 24) {
-            return $diff_hours_count . ' ' . get_noun_plural_form($diff_hours_count, 'час', 'часа',
-                    'часов') . ' назад';
-        }
+    if ($diff_month_count > 0) {
+        return $diff_month_count . ' ' . get_noun_plural_form($diff_month_count, 'месяц',
+                'месяца', 'месяцев') . ' назад';
     }
 
-    if ($diff_days_count > 0 && $diff_days_count < 7) {
-        return $diff_days_count . ' ' . get_noun_plural_form($diff_days_count, 'день', 'дня',
-                'дней') . ' назад';
-    }
-
-    if ($diff_days_count >= 7 && $diff_days_count < 7 * 5) {
+    if ($diff_days_count >= 7) {
         return round($diff_days_count / 7) . ' ' . get_noun_plural_form(round($diff_days_count / 7),
                 'неделя', 'недели', 'недель') . ' назад';
     }
 
+    if ($diff_days_count > 0) {
+        return $diff_days_count . ' ' . get_noun_plural_form($diff_days_count, 'день', 'дня',
+                'дней') . ' назад';
+    }
 
-    return $diff_month_count . ' ' . get_noun_plural_form($diff_month_count, 'месяц',
-            'месяца', 'месяцев') . ' назад';
+    if ($diff_hours_count > 0) {
+        return $diff_hours_count . ' ' . get_noun_plural_form($diff_hours_count, 'час', 'часа',
+                'часов') . ' назад';
+    }
 
+    return $diff_minutes_count . ' ' . get_noun_plural_form($diff_minutes_count, 'минута', 'минуты',
+            'минут') . ' назад';
 }
 
 function get_custom_time_format($time_data)
