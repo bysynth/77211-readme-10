9ca6c05aff1c2ee28c5e0251c08b255aad216940
VK <belsynth@gmail.com>
2019-08-27 19:30:47 +0300

    Добавил в сценарий проверку существования параметра запроса и изменил рендеринг страницы с популярными постами

--- index.php
+++ index.php
@@ -4,10 +4,20 @@ require_once 'helpers.php';
 require_once 'functions.php';
 require_once 'data.php';
 
+$type = NULL;
+
+if (isset($_GET['type'])) {
+    $type = $_GET['type'];
+}
+
+$active_class = 'filters__button--active';
+
 $page_content = include_template('main.php',
     [
         'content_types' => get_content_types($db_connect),
-        'posts' => get_posts($db_connect)
+        'posts' => get_posts($db_connect, $type),
+        'type' => $type,
+        'active_class' => $active_class
     ]);
 
 $layout_content = include_template('layout.php',
