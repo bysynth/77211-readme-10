80f81e4b82cbd1dcd36bad3544410e974494405f
VK <belsynth@gmail.com>
2019-08-28 17:56:11 +0300

    Внес правки в index.php

--- index.php
+++ index.php
@@ -1,28 +1,20 @@
 <?php
 require_once 'init.php';
-require_once 'helpers.php';
-require_once 'functions.php';
-require_once 'data.php';
 
-if (empty($_GET)) {
-    $type = null;
-} else {
-    $type = $_GET['type'];
-};
+$type = $_GET['type'] ?? null;
 
 $content_types = get_content_types($db_connect);
 
-if ($type !== null && is_type_exist($content_types, $type) === false) {
+if ($type === '' || ($type !== null && is_type_exist($content_types, $type) === false)) {
     http_response_code(404);
-    die;
+    exit('Ошибка 404 -- Запрашиваемая страница не найдена');
 }
 
 $page_content = include_template('main.php',
     [
         'content_types' => $content_types,
         'posts' => get_posts($db_connect, $type),
-        'type' => $type,
-        'active_class' => 'filters__button--active'
+        'type' => $type
     ]);
 
 $layout_content = include_template('layout.php',
