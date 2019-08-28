939bcb038f896a15a1aac894867b63b42af60d55
VK <belsynth@gmail.com>
2019-08-28 11:29:55 +0300

    Правки в index.php

--- index.php
+++ index.php
@@ -4,25 +4,31 @@ require_once 'helpers.php';
 require_once 'functions.php';
 require_once 'data.php';
 
-$type = NULL;
-
-if (isset($_GET['type'])) {
+if (empty($_GET)) {
+    $type = null;
+} else {
     $type = $_GET['type'];
-}
+};
 
-$active_class = 'filters__button--active';
+$content_types = get_content_types($db_connect);
+
+if ($type !== null && is_type_exist($content_types, $type) === false) {
+    http_response_code(404);
+    die;
+}
 
 $page_content = include_template('main.php',
     [
-        'content_types' => get_content_types($db_connect),
+        'content_types' => $content_types,
         'posts' => get_posts($db_connect, $type),
         'type' => $type,
-        'active_class' => $active_class
+        'active_class' => 'filters__button--active'
     ]);
 
 $layout_content = include_template('layout.php',
     [
         'content' => $page_content,
+        'main_class' => 'page__main--popular',
         'title' => 'readme: популярные посты',
         'is_auth' => $is_auth,
         'user_name' => $user_name
