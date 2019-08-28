d6bb3fa8157ad337c45e0afd053085277e24295f
VK <belsynth@gmail.com>
2019-08-28 17:57:16 +0300

    Внес правки в post.php

--- post.php
+++ post.php
@@ -1,31 +1,28 @@
 <?php
 require_once 'init.php';
-require_once 'helpers.php';
-require_once 'functions.php';
-require_once 'data.php';
 
-if (empty($_GET)) {
+if (!isset($_GET['id']) || $_GET['id'] === '') {
     http_response_code(404);
-    exit;
+    exit('Ошибка 404 -- Запрашиваемая страница не найдена');
 }
 
-$id = $_GET['id'];
+$id = (int) $_GET['id'];
 
 $post_content = get_post($db_connect, $id);
 
 if (empty($post_content)) {
     http_response_code(404);
-    die;
+    exit('Ошибка 404 -- Запрашиваемая страница не найдена');
 }
 
-$user_subscriptions_count = get_subsriptions_count($db_connect, $post_content['author_id']);
-$user_publications_count = get_publications_count($db_connect, $post_content['author_id']);
+$user_subscriptions_count = isset($post_content['author_id']) ? get_subscriptions_count($db_connect, $post_content['author_id']) : 0;
+$user_publications_count = isset($post_content['author_id']) ? get_publications_count($db_connect, $post_content['author_id']) : 0;
 
 $page_content = include_template('post.php',
     [
         'post_content' => $post_content,
-        'user_subscriptions_count' => $user_subscriptions_count,
-        'user_publications_count' => $user_publications_count
+        'user_subscriptions_count' => $user_subscriptions_count['count'],
+        'user_publications_count' => $user_publications_count['count']
     ]);
 
 $layout_content = include_template('layout.php',
