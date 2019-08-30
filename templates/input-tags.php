287fea526d9255ef027271494e53b0d9fe8cb6c8
VK <belsynth@gmail.com>
2019-08-30 16:55:55 +0300

    Скорректировал атрибут name в шаблонах полей для форм

--- templates/input-tags.php
+++ templates/input-tags.php
@@ -1,6 +1,6 @@
 <label class="adding-post__label form__label" for="<?= $type ?>-tags">Теги</label>
 <div class="form__input-section">
-    <input class="adding-post__input form__input" id="<?= $type ?>-tags" type="text" name="photo-heading" placeholder="Введите теги">
+    <input class="adding-post__input form__input" id="<?= $type ?>-tags" type="text" name="<?= $type ?>-tags" placeholder="Введите теги">
     <button class="form__error-button button" type="button">!<span class="visually-hidden">Информация об ошибке</span></button>
     <div class="form__error-text">
         <h3 class="form__error-title">Заголовок сообщения</h3>
