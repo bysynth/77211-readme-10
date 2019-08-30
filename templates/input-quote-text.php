b965398883447f60e95e65bf47bc4ab06e952f26
VK <belsynth@gmail.com>
2019-08-30 16:49:54 +0300

    Переименовал шаблон для поля текста цитаты

--- templates/input-quote-text.php
+++ templates/input-quote-text.php
@@ -1,6 +1,6 @@
-<label class="adding-post__label form__label" for="cite-text">Текст цитаты <span class="form__input-required">*</span></label>
+<label class="adding-post__label form__label" for="quote-text">Текст цитаты <span class="form__input-required">*</span></label>
 <div class="form__input-section">
-    <textarea class="adding-post__textarea adding-post__textarea--quote form__textarea form__input" id="cite-text" placeholder="Текст цитаты"></textarea>
+    <textarea class="adding-post__textarea adding-post__textarea--quote form__textarea form__input" id="quote-text" name="quote-text" placeholder="Текст цитаты"></textarea>
     <button class="form__error-button button" type="button">!<span class="visually-hidden">Информация об ошибке</span></button>
     <div class="form__error-text">
         <h3 class="form__error-title">Заголовок сообщения</h3>
