45dc30b1850cd3580f5eba48b624cd6fdc42ff7b
VK <belsynth@gmail.com>
2019-08-31 10:19:12 +0300

    Заменил код блока для вывода ошибки выводом шаблона ошибки

--- templates/input-quote-author.php
+++ templates/input-quote-author.php
@@ -2,8 +2,8 @@
 <div class="form__input-section">
     <input class="adding-post__input form__input" id="quote-author" type="text" name="quote-author">
     <button class="form__error-button button" type="button">!<span class="visually-hidden">Информация об ошибке</span></button>
-    <div class="form__error-text">
-        <h3 class="form__error-title">Заголовок сообщения</h3>
-        <p class="form__error-desc">Текст сообщения об ошибке, подробно объясняющий, что не так.</p>
-    </div>
+    <?= include_template('input-error.php', [
+        'error_title' => 'Заголовок',
+        'error_desc' => 'Текст сообщения об ошибке, подробно объясняющий, что не так.'
+    ])?>
 </div>
