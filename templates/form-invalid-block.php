cff8d1ba1519a70aecd7943b8006667e82294d8a
VK <belsynth@gmail.com>
2019-09-03 17:29:29 +0300

    Добавил проверку isset в шаблон form-invalid-block.php

--- templates/form-invalid-block.php
+++ templates/form-invalid-block.php
@@ -2,7 +2,9 @@
     <b class="form__invalid-slogan">Пожалуйста, исправьте следующие ошибки:</b>
     <ul class="form__invalid-list">
         <?php foreach ($errors as $error) : ?>
-            <?= '<li class="form__invalid-item">' . $error['input_name'] . '. ' . $error['input_error_desc'] . '</li>' ?>
+            <?php if (isset($error['input_name'], $error['input_error_desc'])): ?>
+                <?= '<li class="form__invalid-item">' . $error['input_name'] . '. ' . $error['input_error_desc'] . '</li>' ?>
+            <?php endif; ?>
         <?php endforeach; ?>
     </ul>
 </div>
