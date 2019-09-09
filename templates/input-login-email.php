219847d1f7328ca9a506a1a0ea6042aa02f08310
VK <belsynth@gmail.com>
2019-09-09 16:05:58 +0300

    Добавил шаблон для страницы входа и шаблоны полей формы входа.

---
+++ templates/input-login-email.php
@@ -0,0 +1,14 @@
+<div class="login__input-wrapper form__input-wrapper">
+    <label class="login__label form__label" for="login-email">Электронная почта</label>
+    <div class="form__input-section <?= !empty($error) ? 'form__input-section--error' : '' ?>">
+        <input class="login__input form__input" id="login-email" type="email" name="email"
+               placeholder="Укажите эл.почту" value="<?= get_post_val('email') ?>">
+        <button class="form__error-button button" type="button">!<span
+                class="visually-hidden">Информация об ошибке</span></button>
+        <?= include_template('input-error.php',
+            [
+                'error' => $error
+            ])
+        ?>
+    </div>
+</div>
---
+++ templates/login.php
@@ -0,0 +1,19 @@
+<div class="container">
+    <h1 class="page__title page__title--login">Вход</h1>
+</div>
+<section class="login container">
+    <h2 class="visually-hidden">Форма авторизации</h2>
+    <form class="login__form form" action="#" method="post">
+        <?= include_template('input-login-email.php',
+            [
+                'error' => $errors['email'] ?? []
+            ])
+        ?>
+        <?= include_template('input-login-password.php',
+            [
+                'error' => $errors['password'] ?? []
+            ])
+        ?>
+        <button class="login__submit button button--main" type="submit">Отправить</button>
+    </form>
+</section>
